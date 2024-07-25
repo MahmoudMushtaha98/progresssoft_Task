665d5a5aaff3dbeb49f80f84e1c028f723e99877
MahmoudMushtaha98 <modemushtaha@gmail.com>
2024-07-25 08:01:44 +0300

    localization

---
+++ lib/bloc/my_app_event.dart
@@ -0,0 +1,4 @@
+part of 'my_app_bloc.dart';
+
+@immutable
+sealed class MyAppEvent {}
---
+++ lib/bloc/my_app_state.dart
@@ -0,0 +1,6 @@
+part of 'my_app_bloc.dart';
+
+@immutable
+sealed class MyAppState {}
+
+final class MyAppInitial extends MyAppState {}
